a209cc96b956af47c0ed5203f3f76db21fe31837
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 14:02:55 -0500

    Working on event handlers

---
+++ components/Keypad.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Keypad extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <input type="password">
+      </div>
+    )
+  }
+}
+
+
+
+
+module.exports = Keypad;
